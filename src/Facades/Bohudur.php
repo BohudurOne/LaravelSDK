6600dd26f238c29bc37e7b944d5c3b1b9abf7d43
Bohudur <services@bohudur.one>
2026-05-01 09:44:44 +0600

    Update Bohudur.php

--- src/Facades/Bohudur.php
+++ src/Facades/Bohudur.php
@@ -4,7 +4,45 @@ namespace App\Modules\Bohudur\Facades;
 use Illuminate\Support\Facades\Facade;
 
 class Bohudur extends Facade {
+    protected static ?string $apiKey = null;
+
     protected static function getFacadeAccessor() {
         return \App\Modules\Bohudur\Services\BohudurService::class;
+    }
+
+    public static function init(string $apiKey): void {
+        static::$apiKey = $apiKey;
+    }
+
+    public static function request() {
+        $service = app(static::getFacadeAccessor());
+
+        $request = $service->request(static::$apiKey);
+
+        static::$apiKey = null;
+
+        return $request;
+    }
+
+    public static function query(string $paymentKey) {
+        $service = app(static::getFacadeAccessor());
+
+        if (static::$apiKey) {
+            $service->setApiKey(static::$apiKey);
+            static::$apiKey = null;
+        }
+
+        return $service->query($paymentKey);
+    }
+
+    public static function execute(string $paymentKey) {
+        $service = app(static::getFacadeAccessor());
+
+        if (static::$apiKey) {
+            $service->setApiKey(static::$apiKey);
+            static::$apiKey = null;
+        }
+
+        return $service->execute($paymentKey);
     }
 }
